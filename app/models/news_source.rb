3caf17f2627bdec3c0434ebd462d1f2338834ded
towerofl <ofl@covered.jp>
2013-11-05 06:58:08 +0900

    Remove NewsCompany

--- app/models/news_source.rb
+++ app/models/news_source.rb
@@ -10,41 +10,15 @@ class NewsSource
     :cc          => :int,
     :url         => :string,
     :host        => :string,
-    :position    => :int,
     :fetched_at  => :date,
     :image_path  => :string,
     :channels    => :array
-
-  def company
-    Company.where(:cc).eq(self.cc).first
-  end
 
   def crawl_articles
     article_manager = ArticleManager.instance
     article_manager.crawl(self)
     self.fetched_at = Time.now
     self.save
-    self.class.save_to_file
-  end
-
-  def move_position_to(old_pos, new_pos)
-    if new_pos > old_pos
-      objects = self.class.find{ |o| o.channel_id == self.channel_id && o.position > old_pos && o.position <= new_pos }
-      p objects.count
-      objects.each do |o|
-        o.position -= 1
-        o.save
-      end
-    else
-      objects = self.class.find{ |o| o.channel_id == self.channel_id && o.position < old_pos && o.position >= new_pos }
-      p objects.count
-      objects.each do |o|
-        o.position += 1
-        o.save
-      end
-    end
-    self.position = new_pos
-    self.save()
     self.class.save_to_file
   end
 end
